adb581df6252d05019ebdb8fc50b996b84ef9782
Hewkai <fkgthnotv@gmail.om>
2022-12-16 10:07:56 +0700

    4

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,18 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+    double key;
+cout << "Please input GPA: " ;
+	cin >> key ;
+
+	int N= 10;
+	int i=0;
+	while(i<N){
+		if(gpa[i] >= key){
+			cout << id[i] << " " << name[i] << " " << gpa[i] << endl;
+		}
+		i++;
+	}
 		
 	return 0;
 }
